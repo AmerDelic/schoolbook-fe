cfa51ac615ccb8f78830d0456bf6a505363441e7
Amer D <1amerdelic1@gmail.com>
2023-06-18 20:34:41 +0200

    user managment

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.less']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+  isCollapsed = true;
+  private titleSubject = new BehaviorSubject<String>('Users')
+  public titleAction$ = this.titleSubject.asObservable();
+
+  public toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  public changeTitle(title: string): void {
+    this.titleSubject.next(title);
+  }
+
+  ngOnInit(): void {
+  }
 
 }
